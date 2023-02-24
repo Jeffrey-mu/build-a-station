dd45923a299f2575c9184818fa5c5200870a6885
Jeffrey <1115378579@qq.com>
2023-02-24 14:04:03 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="p-4">
+    <Header />
+    <slot />
+  </main>
+</template>
---
+++ layouts/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="p-4">
+    <Header />
+    <slot />
+    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
+      [Home Layout]
+    </div>
+  </main>
+</template>
